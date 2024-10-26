2c649b0ccd0b5c9547815480c9860357e5ae8526
creation_hy <creation.hy1016@gmail.com>
2024-10-27 01:53:44 +0800

    fix: ChatMarkdown 样式问题
    - 解决了 margin 和 padding 设置错误、分割线无法正常使用、代码块无法换行、代码块的空格被替换为“&nbsp;”六个字符的问题，美化了样式。
    
    Signed-off-by: creation_hy <creation.hy1016@gmail.com>

--- src/components/ChatMarkdown.jsx
+++ src/components/ChatMarkdown.jsx
@@ -1,16 +1,19 @@
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import remarkBreaks from "remark-breaks";
 import PropTypes from "prop-types";
+import remarkBreaks from "remark-breaks";
 
 export const ChatMarkdown = ({children, ...props}) => {
 	if (!children)
 		children = "";
 	
+	const md = children.replace(/ {2}/g, " \u00A0")
+		.replace(/(?<![*-])(?<![*-]\n)\n{2,}(?![*-])/g,
+			(match) => "\n\n" + "\u00A0  \n".repeat(match.length - 1));
+	
 	return (
 		<Markdown className="my-markdown" remarkPlugins={[remarkGfm, remarkBreaks]} {...props}>
-			{children.toString().replace(/ {2}/g, " &nbsp;")
-				.replace(/\n{2,}/g, (match) => "\n\n" + "&nbsp;  \n".repeat(match.length - 1))}
+			{md}
 		</Markdown>
 	)
 }
