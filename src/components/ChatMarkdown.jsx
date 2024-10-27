a3c8b0c398ee94e51e674ce3999eaa5a0bffab58
creation_hy <creation.hy1016@gmail.com>
2024-10-28 01:45:36 +0800

    feat: 自定义 ChatMarkdown 转换后的组件
    - 转换后的 hr, a, p, del, em, strong, b, h1-h6, 表格, 列表 全部渲染为 Material UI 对应组件。
    - 转换后的代码块将被渲染为 SyntaxHighlighter，行内代码将被渲染为自定义 code 样式。
    
    Signed-off-by: creation_hy <creation.hy1016@gmail.com>

--- src/components/ChatMarkdown.jsx
+++ src/components/ChatMarkdown.jsx
@@ -1,18 +1,99 @@
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import PropTypes from "prop-types";
+import {List, ListItem, ListItemText, Paper, Table, TableBody, TableCell, TableContainer, TableRow} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Divider from "@mui/material/Divider";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import {tomorrow, tomorrowNight} from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import {useColorMode} from "src/components/ColorMode.jsx";
 import remarkBreaks from "remark-breaks";
 
 export const ChatMarkdown = ({children, ...props}) => {
 	if (!children)
 		children = "";
 	
-	const md = children.replace(/ {2}/g, " \u00A0")
+	const md = children
+		.replace(/(```\s*\n)([\s\S]*?)(\n```)/g, "```auto\n$2\n```")
+		.replace(/ {2}/g, " \u00A0")
 		.replace(/(?<!((?<!\S)([*_-]{3,})(?!\S)))\n{2,}(?!((?<!\S)([*_-]{3,})(?!\S)))/g,
 			(match) => "\n\n" + "\u00A0  \n".repeat(match.length - 1));
+	console.log(md);
+	const [colorMode] = useColorMode();
 	
 	return (
-		<Markdown className="my-markdown" remarkPlugins={[remarkGfm, remarkBreaks]} {...props}>
+		<Markdown
+			className="my-markdown"
+			remarkPlugins={[remarkGfm, remarkBreaks]}
+			components={{
+				hr: () => <Divider sx={{my: 1}}/>,
+				a: ({href, title, children}) => (<Link href={href} title={title}>{children}</Link>),
+				
+				p: ({children}) => (<Typography component="div">{children}</Typography>),
+				del: ({children}) => (<Typography component="div" sx={{textDecoration: "line-through"}}>{children}</Typography>),
+				em: ({children}) => (<Typography component="div" sx={{fontStyle: "italic"}}>{children}</Typography>),
+				strong: ({children}) => (<Typography component="div" sx={{fontWeight: "bold"}}>{children}</Typography>),
+				b: ({children}) => (<Typography component="div" sx={{fontWeight: "bold"}}>{children}</Typography>),
+				h1: ({children}) => (<Typography component="div" variant="h1">{children}</Typography>),
+				h2: ({children}) => (<Typography component="div" variant="h2">{children}</Typography>),
+				h3: ({children}) => (<Typography component="div" variant="h3">{children}</Typography>),
+				h4: ({children}) => (<Typography component="div" variant="h4">{children}</Typography>),
+				h5: ({children}) => (<Typography component="div" variant="h5">{children}</Typography>),
+				h6: ({children}) => (<Typography component="div" variant="h6">{children}</Typography>),
+				
+				table: ({children}) => (<TableContainer component={Paper} sx={{my: 0.5}}>
+					<Table size="small">{children}</Table>
+				</TableContainer>),
+				tbody: ({children}) => (<TableBody>{children}</TableBody>),
+				tr: ({children}) => (<TableRow>{children}</TableRow>),
+				td: ({children}) => (<TableCell><Typography component="div">{children}</Typography></TableCell>),
+				
+				ol: ({children}) => (<List sx={{
+					listStyleType: "decimal",
+					m: 0,
+					p: 0,
+					"& .MuiListItem-root": {display: "list-item"},
+				}}>{children}</List>),
+				ul: ({children}) => (<List sx={{
+					listStyleType: "disc",
+					m: 0,
+					p: 0,
+					"& .MuiListItem-root": {display: "list-item"},
+				}}>{children}</List>),
+				li: ({children}) => (
+					<ListItem sx={{m: 0, p: 0}} disableGutters>
+						<ListItemText>{children}</ListItemText>
+					</ListItem>),
+				
+				code: ({className, children}) => {
+					const match = /language-(\w+)/.exec(className || "");
+					
+					return match ? (
+						<SyntaxHighlighter
+							style={colorMode === "light" ? tomorrow : tomorrowNight}
+							language={match ? match[1] : undefined}
+							PreTag="div"
+							className="syntax-highlighter"
+						>
+							{children.replace(/(?<!\n[\s\u00A0]*)\n(?![\s\u00A0]*\n)/g, "\n\n")}
+						</SyntaxHighlighter>
+					) : (
+						<code style={{
+							color: colorMode === "light" ? "black" : "white",
+							backgroundColor: colorMode === "light" ? "white" : "rgb(29, 31, 33)",
+							borderRadius: 4,
+							padding: "1px 4px",
+							marginLeft: 2,
+							marginRight: 2,
+						}}>
+							{children}
+						</code>
+					);
+				},
+			}}
+			{...props}
+		>
 			{md}
 		</Markdown>
 	)
