f181e292792f19a99e9f1cc3d827d8a3243d77ce
creation_hy <creation.hy1016@gmail.com>
2024-10-29 23:26:39 +0800

    feat: 添加对 gzip 的支持
    - 减缓了客户端获取脚本文件的压力。
    
    Signed-off-by: creation_hy <creation.hy1016@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,32 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import * as path from 'path'
-import {resolve} from 'path'
+import {defineConfig} from "vite"
+import react from "@vitejs/plugin-react"
+import * as path from "path"
+import {resolve} from "path"
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		viteCompression({threshold: 10240, verbose: false}),
+	],
 	server: {
-		host: '0.0.0.0',
+		host: "0.0.0.0",
 		proxy: {
 			"/api": {
-				target: 'https://localhost:8080/',
+				target: "https://localhost:8080/",
 				secure: false,
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, ""),
 			},
 			"/avatars": {
-				target: 'https://localhost:8080/avatars/',
+				target: "https://localhost:8080/avatars/",
 				secure: false,
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/avatars/, ""),
 			},
 			"/api/websocket": {
-				target: 'wss://localhost:8080/websocket',
+				target: "wss://localhost:8080/websocket",
 				secure: false,
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api\/websocket/, ""),
@@ -31,16 +35,15 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			'src': resolve(path.resolve(), 'src'),
+			"src": resolve(path.resolve(), "src"),
 		},
 	},
 	build: {
 		rollupOptions: {
 			output: {
 				manualChunks(id) {
-					if (id.includes('node_modules')) {
+					if (id.includes("node_modules"))
 						return "vendor";
-					}
 				},
 			},
 		},
