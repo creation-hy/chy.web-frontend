d4d75d979bdd5be28d149d22b35933e213b17f52
creation_hy <creation.hy1016@gmail.com>
2024-11-08 16:46:49 +0800

    feat: Chat 在线提示（右下角绿点）美化
    Signed-off-by: creation_hy <creation.hy1016@gmail.com>

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -52,6 +52,7 @@ import {useClientUser} from "src/components/ClientUser.jsx";
 import {convertDateToLocaleAbsoluteString, convertDateToLocaleShortString} from "src/assets/DateUtils.jsx";
 import SignUp from "src/pages/SignUp.jsx";
 import {UserAvatar} from "src/components/UserAvatar.jsx";
+import getDefaultTheme from "src/theme/getDefaultTheme.jsx";
 
 const myname = Cookies.get("username"), myToken = Cookies.get("user_token");
 
@@ -70,11 +71,32 @@ function UserItem({username, displayName, isOnline, newMessageCount, lastMessage
 						badgeContent={isOnline === true ? " " : 0} overlap="circular" color="success" variant="dot"
 						anchorOrigin={{vertical: "bottom", horizontal: "right"}}
 						sx={{
-							"& .MuiBadge-badge": {
+							'& .MuiBadge-badge': {
 								backgroundColor: '#44b700',
 								color: '#44b700',
-								boxShadow: `0 0 0 2px`,
-							}
+								boxShadow: `0 0 0 2px ${getDefaultTheme(colorMode).palette.background.paper}`,
+								'&::after': {
+									position: 'absolute',
+									top: 0,
+									left: 0,
+									width: '100%',
+									height: '100%',
+									borderRadius: '50%',
+									animation: 'ripple 1.2s infinite ease-in-out',
+									border: '1px solid currentColor',
+									content: '""',
+								},
+							},
+							'@keyframes ripple': {
+								'0%': {
+									transform: 'scale(.8)',
+									opacity: 1,
+								},
+								'100%': {
+									transform: 'scale(2.4)',
+									opacity: 0,
+								},
+							},
 						}}
 					>
 						<UserAvatar username={username} displayName={displayName}/>
