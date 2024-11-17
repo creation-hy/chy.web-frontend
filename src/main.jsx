0980888d5b6e9bd195f04dfbe1c8fe02dd14ab4a
creation_hy <creation.hy1016@gmail.com>
2024-11-18 03:24:35 +0800

    refactor: 启用 React 严格模式
    Signed-off-by: creation_hy <creation.hy1016@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -4,21 +4,22 @@ import './index.css'
 import {ColorModeProvider} from "src/components/ColorMode.jsx";
 import {ClientUserProvider} from "src/components/ClientUser.jsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {StrictMode} from "react";
 
 createRoot(document.getElementById('root')).render(
-	//<StrictMode>
-	<QueryClientProvider client={new QueryClient({
-		defaultOptions: {
-			queries: {
-				staleTime: 60 * 1000,
+	<StrictMode>
+		<QueryClientProvider client={new QueryClient({
+			defaultOptions: {
+				queries: {
+					staleTime: 60 * 1000,
+				},
 			},
-		},
-	})}>
-		<ClientUserProvider>
-			<ColorModeProvider>
-				<App/>
-			</ColorModeProvider>
-		</ClientUserProvider>
-	</QueryClientProvider>
-	//</StrictMode>,
+		})}>
+			<ClientUserProvider>
+				<ColorModeProvider>
+					<App/>
+				</ColorModeProvider>
+			</ClientUserProvider>
+		</QueryClientProvider>
+	</StrictMode>,
 )
