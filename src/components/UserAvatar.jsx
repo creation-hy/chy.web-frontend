c27b6dd305b88e571aeb5fe3a8355d707dd3fe7c
creation_hy <creation.hy1016@gmail.com>
2024-11-19 04:29:10 +0800

    feat: UserAvatar 组件添加对 avatar version 的支持
    Signed-off-by: creation_hy <creation.hy1016@gmail.com>

--- src/components/UserAvatar.jsx
+++ src/components/UserAvatar.jsx
@@ -2,12 +2,13 @@ import PropTypes from "prop-types";
 import Avatar from "@mui/material/Avatar";
 import {memo} from "react";
 
-export const UserAvatar = memo(({username, displayName, width, height, ...props}) => {
+export const UserAvatar = memo(({username, displayName, avatarVersion, width, height, ...props}) => {
 	return (
-		<Avatar src={`/avatars/${username}.webp`}
-		        alt={displayName}
-		        sx={{width: width, height: height}}
-		        {...props}
+		<Avatar
+			src={`/avatars/${username}.webp?v=${avatarVersion}`}
+			alt={displayName}
+			sx={{width: width, height: height}}
+			{...props}
 		/>
 	);
 });
@@ -15,6 +16,7 @@ export const UserAvatar = memo(({username, displayName, width, height, ...props}
 UserAvatar.propTypes = {
 	username: PropTypes.string.isRequired,
 	displayName: PropTypes.string,
+	avatarVersion: PropTypes.number.isRequired,
 	width: PropTypes.number,
 	height: PropTypes.number,
 }
