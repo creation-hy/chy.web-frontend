b4d19c711a1fec3a01b842e518cb41fa97341d0c
creation_hy <creation.hy1016@gmail.com>
2024-11-19 16:30:59 +0800

    feat: SimpleUserItem 添加 disableNavigate 选项
    Signed-off-by: creation_hy <creation.hy1016@gmail.com>

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -6,12 +6,12 @@ import PropTypes from "prop-types";
 import {memo} from "react";
 import {useNavigate} from "react-router";
 
-export const SimpleUserItem = memo(({username, displayName, avatarVersion, ...props}) => {
+export const SimpleUserItem = memo(({username, displayName, avatarVersion, disableNavigate = false, ...props}) => {
 	const navigate = useNavigate();
 	
 	return (
 		<Grid container wrap="nowrap" alignItems="center" spacing={1} height="100%" {...props}>
-			<Link onClick={() => navigate(`/user/${username}`)} underline="none">
+			<Link onClick={disableNavigate ? undefined : () => navigate(`/user/${username}`)} underline="none" sx={{cursor: "pointer"}}>
 				<UserAvatar username={username} displayName={displayName} avatarVersion={avatarVersion}/>
 			</Link>
 			<Typography
@@ -20,7 +20,7 @@ export const SimpleUserItem = memo(({username, displayName, avatarVersion, ...pr
 				noWrap
 				overflow="hidden"
 				textOverflow="ellipsis"
-				onClick={() => navigate(`/user/${username}`)}
+				onClick={disableNavigate ? undefined : () => navigate(`/user/${username}`)}
 				sx={{cursor: "pointer"}}
 			>
 				{displayName}
@@ -33,4 +33,5 @@ SimpleUserItem.propTypes = {
 	username: PropTypes.string.isRequired,
 	displayName: PropTypes.string,
 	avatarVersion: PropTypes.number.isRequired,
+	disableNavigate: PropTypes.bool,
 }
