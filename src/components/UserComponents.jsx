a89ee1d485ca71ce074b539d9c27824333b9df11
creation_hy <creation.hy1016@gmail.com>
2024-11-20 05:10:51 +0800

    feat: UserBadge 组件
    Signed-off-by: creation_hy <creation.hy1016@gmail.com>

--- src/components/UserComponents.jsx
+++ src/components/UserComponents.jsx
@@ -5,6 +5,8 @@ import PropTypes from "prop-types";
 import {memo} from "react";
 import {useNavigate} from "react-router";
 import Avatar from "@mui/material/Avatar";
+import {Verified} from "@mui/icons-material";
+import SvgIcon from "@mui/material/SvgIcon";
 
 export const UserAvatar = memo(({username, displayName, avatarVersion, width, height, ...props}) => {
 	return (
@@ -23,6 +25,34 @@ UserAvatar.propTypes = {
 	avatarVersion: PropTypes.number.isRequired,
 	width: PropTypes.number,
 	height: PropTypes.number,
+}
+
+export const UserBadge = memo(({badge, ...props}) => {
+	if (!badge) {
+		return null;
+	} else if (badge === "Official") {
+		return <Verified color="secondary" {...props}/>;
+	} else if (badge === "User1") {
+		return <Verified color="primary" {...props}/>;
+	} else if (badge === "User2") {
+		return <Verified color="warning" {...props}/>;
+	} else if (badge === "Transgender") {
+		return (
+			<SvgIcon {...props}>
+				<defs>
+					<linearGradient id="transgender-badge-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+						<stop offset="25%" style={{stopColor: '#f5a9b8', stopOpacity: 1}}/>
+						<stop offset="100%" style={{stopColor: '#5bcefa', stopOpacity: 1}}/>
+					</linearGradient>
+				</defs>
+				<Verified sx={{fill: 'url(#transgender-badge-gradient)'}}/>
+			</SvgIcon>
+		);
+	}
+});
+
+UserBadge.propTypes = {
+	badge: PropTypes.string,
 }
 
 export const SimpleUserItem = memo(({username, displayName, avatarVersion, disableNavigate = false, ...props}) => {
