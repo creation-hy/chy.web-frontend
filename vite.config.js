dfa007b36e1ddf7445238fb6ccce763db5559208
creation_hy <creation.hy1016@gmail.com>
2025-02-23 16:37:47 +0800

    refactor: 删除了 vite-plugin-compression，更新了依赖项
    Signed-off-by: creation_hy <creation.hy1016@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,10 @@ import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 import * as path from "path"
 import {resolve} from "path"
-import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		viteCompression({threshold: 10240, verbose: false}),
-	],
+	plugins: [react()],
 	server: {
 		host: "0.0.0.0",
 		proxy: {
